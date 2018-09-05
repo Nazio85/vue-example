62cae43f6178fe1c0037fa9d1871e8340f349052
hairutdinov <hairutdinov@skpari.ru>
2018-09-05 16:36:30 +0300

    init

---
+++ src/views/product/ProductDetail.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-card ma-1>
+        <v-card-title>
+            <div>
+                <h4 class="">Name: </h4>
+            </div>
+        </v-card-title>
+        <v-card-text>
+            <div>
+                <p>Price: руб.</p>
+                <p>Кол-во: кг.</p>
+            </div>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+    export default {
+        name: "ProductItem"
+    }
+</script>
+
+<style scoped>
+
+</style>
